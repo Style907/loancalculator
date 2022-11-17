e30b3a6f93570542196d4b364258b480bd4e02ca
Logan Lanni <style907@Logans-Air.hsd1.or.comcast.net>
2022-11-16 21:36:47 -0800

    made the math work, made input values work, need to make update monthly work

--- calculator.js
+++ calculator.js
@@ -10,31 +10,56 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 
 function getCurrentUIValues() {
-  return {
+  values = {
     amount: +(document.getElementById("loan-amount").value),
     years: +(document.getElementById("loan-years").value),
     rate: +(document.getElementById("loan-rate").value),
   }
+  
 }
 
 // Get the inputs from the DOM.
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  
+  
+  
+  
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  getCurrentUIValues();
+  calculateMonthlyPayment(values)
+  
+  
+  
+  
+
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const arr = Object.values(values)
+  let p = arr[0]
+  let n = arr[1] * 12
+  let i = (arr[2]/100) / 12
+  let x = Math.pow((1 + i), -n)
+  let num = (p*i) / (1-x)
+  let total = Math.floor(num*100)/100
+  
+  
+  
+  
+  
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  
 }
